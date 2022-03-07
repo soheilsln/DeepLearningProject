c33d5fd53beee45828cef6353ea1af3aa7d24b3b
Tarun Manne <94223311+tarunmanne@users.noreply.github.com>
2022-03-07 20:16:43 +0000

    RoomSpawnerUpdate
    Room Spawner

---
+++ Assets/Scripts/RoomTemplates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour {
+
+	public GameObject[] bottomRooms;
+	public GameObject[] topRooms;
+	public GameObject[] leftRooms;
+	public GameObject[] rightRooms;
+
+	public GameObject closedRoom;
+
+	public List<GameObject> rooms;
+
+	public float waitTime;
+
+	void Update(){
+
+			waitTime -= Time.deltaTime;
+		}
+
+}
+
