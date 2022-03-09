58222e12193b4f252b70d1d938d70cc8974366fb
soheilsln <soheil.salehinia@gmail.com>
2022-03-09 18:26:47 +0000

    Level Generator finished

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private string uri = "https://levelgenerator.herokuapp.com";
+    private string level = "";
+    private int[,] levelMatrix;
+
+    public GameObject[] prefabs;
+
+    private IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            switch (webRequest.result)
+            {
+                case UnityWebRequest.Result.ConnectionError:
+                case UnityWebRequest.Result.DataProcessingError:
+                    Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.ProtocolError:
+                    Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+                    break;
+                case UnityWebRequest.Result.Success:
+                    Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                    level = webRequest.downloadHandler.text;
+                    levelMatrix = LoadLevelMatrix(level);
+                    CreateLevel(levelMatrix);
+                    break;
+            }
+        }
+    }
+
+    private int[,] LoadLevelMatrix(string level)
+    {
+        int[,] levelMatrix = new int[128, 128];
+        string[] values = Regex.Split(level, @"\D+");
+
+        int parseIndex = 0;
+        for (int i = 0; i < levelMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < levelMatrix.GetLength(1); j++)
+            {
+                while (string.IsNullOrEmpty(values[parseIndex]))
+                    parseIndex++;
+
+                levelMatrix[i, j] = int.Parse(values[parseIndex]);
+                parseIndex++;
+            }
+        }
+
+        return levelMatrix;
+    }
+
+    public void GenerateLevel()
+    {
+        StartCoroutine(GetRequest(uri));
+    }
+
+    private void CreateLevel(int[,] levelMatrix)
+    {
+        foreach (Transform child in this.transform)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < levelMatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < levelMatrix.GetLength(1); j++)
+            {
+                Instantiate(prefabs[levelMatrix[i, j]], new Vector3(i, prefabs[levelMatrix[i, j]].transform.position.y, j), Quaternion.identity, this.transform);
+            }
+        }
+    }
+}
